ff5de9707073d1803dd8cbf98cf6db2387c6b443
Sebabin <sebastiancrespin279@gmail.com>
2021-12-08 17:28:08 -0700

    all questions and validation complete

--- app.js
+++ app.js
@@ -43,9 +43,22 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'would you like to enter some onformation about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide som information about yourself'
+      message: 'Provide some information about yourself',
+      when: ({confirmAbout}) => {
+        if(confirmAbout) {
+          return true;
+        }else {
+          return false;
+        }
+      }
     }
   ])
 } ;
